6fe74db04f403648c78e5b46c521498c448aebea
Marticore <martin@tinhydra.com>
2021-02-24 12:57:27 +0100

    First Push all project files

---
+++ 4x Project/Assets/Scripts/Hex.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  The Hex class defines the grid prosition, world space position, size, neighbors, etc... of a Hex tile.
+///  The Hex class does NOT interact with Unity directly in any way (only returns a vector 3 for the correct world space position of a hex)
+/// </summary>
+public class Hex
+{
+    // when using cubic coordinates to define your hex you will need 3 coordinates q,r,s
+    // hexes should never change its position on the hex grid, so use readonly (meaning value can only be set once during the constructor)
+    
+
+    //we would expect 3 values for Hex, but we only need two (given the math above the public fields) we never have to ask for the value of S
+    public Hex(int q, int r)
+    {
+        this.Q = q;
+        this.R = r;
+        this.S = -(q + r);
+    }
+
+    //Q + R + S = 0
+    // S = -(Q+R)
+    public readonly int Q; // Column
+    public readonly int R; // Row
+    public readonly int S; // Some Sum
+
+    readonly float WIDTH_MULTIPLIER = Mathf.Sqrt(3) / 2;
+
+    /// <summary>
+    /// Returns the worldspace position of this hex
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 Position()
+    {
+        float radius = 1f;
+        float height = radius * 2;
+        //setup WIDTH MULTIPLIER as a constant above rather than doing that square root operation with each instantiation
+        float width = WIDTH_MULTIPLIER * height;
+
+        //define spacing between tiles
+        float vert = height * 0.75f;
+        float horiz = width;
+
+
+        return new Vector3(
+            horiz * (this.Q + this.R/2f), //horizontal spacing multiplied by a combination of what Column we're in plus half the Row (drawing rows on a diagonal  between z * x)
+            0,
+            vert * this.R //vertical spacing multilied by the Row we are in
+        );
+    }
+}
---
+++ 4x Project/Assets/Scripts/HexMap.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateMap();
+    }
+    public GameObject HexPrefab;
+    public void GenerateMap() 
+    {
+        //generate our hexes for the map
+        //start by making 10x10 map (will make this a variable later)
+        //10 columns
+        for (int column = 0; column < 10; column++) 
+        {
+            //10 rows
+            for (int row = 0; row < 10; row++)
+            {
+                //Instantiate a Hex
+                    //This will use a helper class (Hex)that is responsible for defining "A Hex" and applies a cube coordinate approach to hex placement.
+                    // Hex.cs is a pure c-sharp data class
+                    // For more on Cube Coordinate approach checkout www.redblobgames.com/grids/hexagons/
+                    //because of the offset in the Hex.cs class we will get a rombus shape as all columns will be drawn on a diagonal
+                        //keep in mind this will result in the max row number being offset from the verticle on the map, ie hex coordinates (0,10) will be +5 units (in world coordinates) along the x axis from hex (0,0) 
+                Hex h = new Hex(column, row);
+
+                // rotation required by this instantiation, using Quaternion.identity means no rotation
+                // fourth parameter (the parent) is optional
+                Instantiate(
+                    HexPrefab,
+                    //new Vector3(column, 0, row), replaced by hex position, h.Position(), from Hex.cs
+                    h.Position(),
+                    Quaternion.identity,
+                    this.transform // pass the HexMap's transform as the parent of the newly created Hexes (everything will be a child of hex map)
+                );
+            }
+            
+        }
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
